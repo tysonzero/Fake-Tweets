a08ab10bc5eabb6f20804c92ea4c0ac5a5903ee7
Chris Rodowicz <crodowicz@college.harvard.edu>
2018-03-13 01:56:15 -0400

    Added ApprovalRatingBar that visually decreases in value over time

---
+++ Assets/Scripts/ApprovalRatingBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ApprovalRatingBar : MonoBehaviour {
+
+    public Image approvalRatingBar_Fill;
+    public float approvalDecrementScalar = 0.01f;
+
+    public float approvalRating;
+
+	// Use this for initialization
+	void Start () {
+        approvalRating = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        approvalRating -= approvalDecrementScalar * Time.deltaTime;
+        approvalRatingBar_Fill.fillAmount = approvalRating;
+
+    }
+}
