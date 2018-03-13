8d4c968f4da6b6f52fa84293720eb8af3e293c95
Chris Rodowicz <crodowicz@college.harvard.edu>
2018-03-13 02:44:34 -0400

    End game state when approval runs out - ability to reload the level

--- Assets/Scripts/ApprovalRatingBar.cs
+++ Assets/Scripts/ApprovalRatingBar.cs
@@ -5,31 +5,42 @@ using UnityEngine.UI;
 
 public class ApprovalRatingBar : MonoBehaviour {
 
+    public GameObject endGamePanel;
+
     public Image approvalRatingBar_Fill;
     public float approvalDecrementScalar = 0.01f;
 
     public float approvalRating;
 
+    public bool isFired;
+
 	// Use this for initialization
 	void Start ()
     {
         approvalRating = 1.0f;
+        isFired = false;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        approvalRating -= approvalDecrementScalar * Time.deltaTime;
-        approvalRatingBar_Fill.fillAmount = approvalRating;
-        if(approvalRating <= 0)
+        if(!isFired)
         {
-            TriggerGameEndState();
-        }
+            approvalRating -= approvalDecrementScalar * Time.deltaTime;
+            BoundApprovalRating();
+            approvalRatingBar_Fill.fillAmount = approvalRating;
+            if (approvalRating <= 0.0f)
+            {
+                TriggerGameEndState();
+            }
+        }      
     }
 
     void TriggerGameEndState()
     {
-
+        isFired = true;
+        endGamePanel.SetActive(true);
+        Debug.Log("YOU'RE FIRED!");
     }
 
     void BoundApprovalRating()
